d4bf7d86b039ff5b3733b917570ee6b376f1f338
Nathan Hencky <nhencky@gmail.com>
2020-10-14 19:59:31 -0700

    stylized the log in page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Alert from "./Alert";
+import "./Login.css";
 
 class Login extends Component {
     state = { err: "" };
@@ -23,40 +24,46 @@ class Login extends Component {
 
         render() {
         return (
-            <div className="w3-card-4" style={{ margin: "2rem" }}>
-                <div className="w3-container w3-blue w3-center w3-xlarge">
-                    Login
-                </div>
-                <div className="w3-container">
+            <div className="grandParentContaniner">
+                <div className="parentContainer">
+                    <form onSubmit={this.login}>
+                        <h1>Welcome to Scry</h1>
+                        <div className="inset">
+                        <p>
+                            <label htmlFor="email">EMAIL ADDRESS</label>
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                            />
+                        </p>
+                        <p>
+                            <label htmlFor="password">PASSWORD</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                            />
+                        </p>
+                        <div className="forgot">
+                <a href="reset_password" style={{color: '#0d93ff'}}>Forgot Password?</a>
+              </div>
+                        <p>
+                            <button type="submit">
+                                Sign In
+                            </button>
+                            {this.state.login && <p>You're logged in!</p>}
+                        </p>
+                        <p className="signUp">
+                Dont have an account? <a href="sign_up" style={{color: '#0d93ff'}}>SIGN UP</a>
+              </p>
+                        </div>
+                    </form>
                     {this.state.err.length > 0 && (
                         <Alert
                             message={`Check your form and try again! (${this.state.err})`}
                         />
                     )}
-                    <form onSubmit={this.login}>
-                        <p>
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                class="w3-input w3-border"
-                                id="email"
-                            />
-                        </p>
-                        <p>
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                class="w3-input w3-border"
-                                id="password"
-                            />
-                        </p>
-                        <p>
-                            <button type="submit" class="w3-button w3-blue">
-                                Login
-                            </button>
-                            {this.state.login && <p>You're logged in!</p>}
-                        </p>
-                    </form>
                 </div>
             </div>
         );
