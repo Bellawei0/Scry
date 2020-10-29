2c2f1bf0102b6033464158863c61a9d028dca7ff
Nathan Hencky <nhencky@gmail.com>
2020-10-29 14:43:57 -0700

    added chart of forecast to display

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Axios from "axios";
+import { JsonToTable } from "react-json-to-table";
+
+
 
 class Forecast extends React.Component {
-    state = {graphURL: "hello", isLoading: true}
+    state = {graphURL: "hello", isLoading: true, responsey: ""}
 
     componentDidMount()
     {
@@ -13,8 +16,9 @@ class Forecast extends React.Component {
             }
         }).then(res =>
     	{
-    		//this.setState({graphURL: res.data})
-    		this.setState({isLoading: false})
+    		this.setState({graphURL: (res.data.success)})
+    		this.setState({responsey: (res.data)})
+            this.setState({isLoading: false})
     	})
     }
 
@@ -28,7 +32,9 @@ class Forecast extends React.Component {
     	return (
         <React.Fragment>
             <div className="w3-container w3-center" style={{margin: "3rem"}}>
-                <h1 className="w3-jumbo">{String(this.state.isLoading)}</h1>
+                <h1 className="w3-jumbo">{this.state.isLoading}</h1>
+                <img alt="derp" src={this.state.graphURL} />
+                <JsonToTable json={this.state.responsey} />
                 <button type="button" className="w3-button w3-blue" onClick={() => window.location = "/"}>&laquo; Back</button>
             </div>
         </React.Fragment>
