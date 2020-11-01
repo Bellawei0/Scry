82ddfd263fa25ad63060dc10d431a6d8d914c951
AndrewMurillo <23265028+AndrewMurillo@users.noreply.github.com>
2020-11-01 11:29:27 -0800

    Changed Login.jsx
    Changed format of Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,37 +32,44 @@ class Login extends Component {
             <div className="grandParentContaniner">
                 <div className="parentContainer">
                     <form onSubmit={this.login}>
-                        <h1>Welcome to Scry</h1>
-                        <div className="inset">
-                        <p>
-                            <label htmlFor="email">EMAIL ADDRESS</label>
-                            <input
-                                type="text"
-                                name="email"
-                                id="email"
-                            />
-                        </p>
-                        <p>
-                            <label htmlFor="password">PASSWORD</label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                            />
-                        </p>
-                        <div className="forgot">
-                <a href="reset_password" style={{color: '#0d93ff'}}>Forgot Password?</a>
-              </div>
-                        <p>
-                            <button type="submit">
-                                Sign In
-                            </button>
-                            {this.state.login && <p>You're logged in!</p>}
-                        </p>
-                        <p className="signUp">
-                Dont have an account? <a href="sign_up" style={{color: '#0d93ff'}}>SIGN UP</a>
-              </p>
-                        </div>
+                        <h1 className="w3-center">Welcome to Scry</h1>
+						<div className="w3-card-4" style={{ margin: "2rem" }}>
+							<div className="w3-container w3-blue w3-center w3-xlarge">
+							Login
+							</div>
+							<div className="w3-container">
+							<p>
+								<label htmlFor="email">Email</label>
+								<input
+									type="text"
+									name="email"
+									class="w3-input w3-border"
+									id="email"
+								/>
+							</p>
+							<p>
+								<label htmlFor="password">Password</label>
+								<input
+									type="password"
+									name="password"
+									class="w3-input w3-border"
+									id="password"
+								/>
+							</p>
+							<div className="forgot">
+								<a href="reset_password" style={{color: '#0d93ff'}}>Forgot Password?</a>
+							</div>
+							<p>
+								<button type="submit" class="w3-button w3-blue">
+									Sign In
+								</button>
+								{this.state.login && <p>You're logged in!</p>}
+							</p>
+							<p className="signUp">
+								Dont have an account? <a href="sign_up" style={{color: '#0d93ff'}}>SIGN UP</a>
+							</p>
+							</div>
+						</div>
                     </form>
                     {this.state.err.length > 0 && (
                         <Alert
