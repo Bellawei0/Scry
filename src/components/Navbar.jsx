1613f23c99563371a8b385240096b2710b6fb020
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-02 11:32:07 -0800

    changed name to Scry in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div className="w3-bar w3-black">
             <a className="w3-bar-item w3-button" href="/">
-                Quickr
+                Scry
             </a>
             <div style={{ float: "right" }}>
                 <a className="w3-bar-item w3-button" href="/login">
