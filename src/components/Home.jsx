78ca92085321e899e2f9e3669eee6a3a98798f25
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-09 21:17:01 -0800

    redesigned the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,141 @@
 // src/components/Home.jsx
 import React from "react";
 import "./app.css";
+import team1 from "./assets/img/team1.png";
 
 function Home() {
     return (
         <React.Fragment>
-            <div
-                className="w3-container w3-black"
-                className ="showcase"
-                style={{ padding: "20rem" }}>
-                <h1 className="w3-text-white">Welcome to Scry</h1>
-                
-                <h2 className="w3-text-white"> Data is a precious thing and will last longer than the system themselves</h2>
-                <p>
+            <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Welcome to Scry</div>
+        <div className="intro-heading text-uppercase">THE WORLD IS ONE BIG DATA PROBLEM</div>
+        {/* <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a> */}
+      </div>
+    </div>
+  </header>
+            <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+          <h3 className="section-subheading text-muted">True Engineers are always together in spirit.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={ require('./assets/img/team1.png') } alt=""/>
+            <h4>Nathan Hencky</h4>
+            <p className="text-muted">Lead Developer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={ require('./assets/img/team2.png') }  alt=""/>
+            <h4>Bella Wei</h4>
+            <p className="text-muted">Lead Designer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src={ require('./assets/img/team3.png') } alt=""/>
+            <h4>Andrew Murillo</h4>
+            <p className="text-muted">Lead Designer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-facebook-f"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="fa fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+          <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        </div>
+      </div>
+    </div>
+  </section>
 
-                </p>
-            </div>
+  
+  <section className="py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt=""/>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6">
+          <a href="#something">
+            <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt=""/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+            
+
+   
 
    
 
