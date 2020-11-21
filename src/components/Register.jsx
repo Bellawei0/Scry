127db1bae95d1805fd4580d0f69806f9823ecbd9
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-20 23:00:09 -0800

    styled register page UI

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,73 +4,91 @@ import axios from "axios";
 import Alert from "./Alert";
 
 class Register extends Component {
-    state = { err: "" };
-    register = (e) => 
-    {
-        e.preventDefault();
-        axios.post("/api/register", 
-        {
-                email: document.getElementById("email").value,
-                username: document.getElementById("username").value,
-                pwd: document.getElementById("password").value,
-        })
-        .then((res) => 
-        {
-            if (res.data.error) 
-            {
-                this.setState({ err: res.data.error });
-            } 
-            else 
-            {
-                window.location = "/login"
-            }
-        });
-    };
+  state = { err: "" };
+  register = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/register", {
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        pwd: document.getElementById("password").value,
+      })
+      .then((res) => {
+        if (res.data.error) {
+          this.setState({ err: res.data.error });
+        } else {
+          window.location = "/login";
+        }
+      });
+  };
 
-    render() 
-    {
-        return (
-            <div className="w3-card-4" style={{ margin: "2rem" }}>
-                <div className="w3-container w3-blue w3-center w3-xlarge">
-                    REGISTER
+  render() {
+    return (
+      <div class="showcase">
+        <div className="grandParentContaniner">
+          <div class="w3-cell-row">
+            <div class="w3-third w3-container w3-Deep Purple">
+              <p></p>
+            </div>
+
+            <div class="w3-third w3-container w3-black">
+              <div className="w3-card-4" style={{ margin: "2rem" }}>
+                <div className="w3-container w3-black w3-center w3-xlarge">
+                  <h1 className="w3-center">REGISTER</h1>
                 </div>
                 <div className="w3-container">
-                    {this.state.err.length > 0 && (<Alert message={`Check your form and try again! (${this.state.err})`}/>)}
-                    <form onSubmit={this.register}>
-                        <p>
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                class="w3-input w3-border"
-                                id="email"
-                            />
-                        </p>
-                        <p>
-                            <label htmlFor="username">Username</label>
-                            <input
-                                type="text"
-                                class="w3-input w3-border"
-                                id="username"
-                            />
-                        </p>
-                        <p>
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                class="w3-input w3-border"
-                                id="password"
-                            />
-                        </p>
-                        <p>
-                            <button type="submit" class="w3-button w3-blue">
-                                Register
-                            </button>
-                        </p>
-                    </form>
+                  {this.state.err.length > 0 && (
+                    <Alert
+                      message={`Check your form and try again! (${this.state.err})`}
+                    />
+                  )}
+                  <form onSubmit={this.register}>
+                    <p>
+                      <label htmlFor="email">Email</label>
+                      <input
+                        type="email"
+                        class="w3-input w3-border"
+                        id="email"
+                      />
+                    </p>
+                    <p>
+                      <label htmlFor="username">Username</label>
+                      <input
+                        type="text"
+                        class="w3-input w3-border"
+                        id="username"
+                      />
+                    </p>
+                    <p>
+                      <label htmlFor="password">Password</label>
+                      <input
+                        type="password"
+                        class="w3-input w3-border"
+                        id="password"
+                      />
+                    </p>
+                    <p>
+                      <button type="submit" class="w3-button w3-blue">
+                        Register
+                      </button>
+                    </p>
+                  </form>
                 </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+          <div>
+            <img
+              className="mx-auto rounded-circle"
+              src={require("./assets/img/space.jpg")}
+              alt=""
+            />
+            <h1>hele</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Register;
