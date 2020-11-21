4f1c2d6fafe142e1e2f23f9fa146d85efde2fdcc
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-20 23:01:25 -0800

    modified to style UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+//import "./components/assets/css/index.css";
 import "./components/assets/css/bootstrap.min.css";
 import "./components/assets/css/agency.min.css";
 
